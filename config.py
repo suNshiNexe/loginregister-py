d57e8037a22678fa5015ff99536b42ce1fcc84c6
suNshiNexe <yuridorea09@gmail.com>
2025-10-11 19:57:06 -0300

    feat: add variáveis jwt

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
+    # DB Variables
     DATABASE_URL: str
+
+    # JWT Variables
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file=".env")
 
