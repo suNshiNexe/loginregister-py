c4f72d1f768e4adb04e171bcb379f1d6290bbfb9
suNshiNexe <yuridorea09@gmail.com>
2025-10-11 20:00:44 -0300

    feat: funtion para gerar token

--- auth.py
+++ auth.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from .config import settings #
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +12,20 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     ## Retorna hash --> texto
     return pwd_context.hash(password)
+
+# Token
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(hours=1)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
